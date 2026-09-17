e0ffa2ff30406529ab9e8ee53f59badc4e33ea0a
BrunaBorbam <bruna.m.borbaa@gmail.com>
2026-09-17 13:17:45 -0300

    fix: removed global overflow-x: hidden and fixed cursor jumping on hover

--- components/CustomCursor.tsx
+++ components/CustomCursor.tsx
@@ -50,8 +50,8 @@ export default function CustomCursor() {
       <motion.div
         className="fixed top-0 left-0 w-6 h-6 bg-white rounded-full pointer-events-none z-[9999] mix-blend-difference flex items-center justify-center"
         animate={{
-          x: mousePosition.x - (isHovering ? 24 : 12),
-          y: mousePosition.y - (isHovering ? 24 : 12),
+          x: mousePosition.x - 12,
+          y: mousePosition.y - 12,
           scale: isHovering ? 2 : 1,
         }}
         transition={{
